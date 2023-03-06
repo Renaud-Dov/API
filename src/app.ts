6f8e48b11bd77f01b25d706cdab4a9ad48f5cc18
Dov Devers <contact@bugbear.fr>
2023-03-06 16:56:41 +0100

    removed docker routes

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ app.use(express.static('src/public'));
 const indexRouter = require('./routes');
 const dockerRouter = require('./routes/docker');
 app.use('/', indexRouter);
-app.use('/docker', dockerRouter);
+// app.use('/docker', dockerRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req: Request, res: Response) {
